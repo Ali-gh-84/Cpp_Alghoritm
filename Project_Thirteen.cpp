68c895e954dce1d7d29a33a5b6672545e4aaffcc
ali-gh-84 <alighafari.a.84@gmail.com>
2024-10-21 21:59:49 +0330

    fix the bug and add future

--- Project_Thirteen.cpp
+++ Project_Thirteen.cpp
@@ -16,18 +16,6 @@ int arr[4][4] = {{1,5,8,6},{3,4,2,1},{2,5,6,8},{7,4,7,3}};  // {{(0,0),2,3,(0,3)
 char table [4][4] = {{'*','*','*','*'},{'*','*','*','*'},{'*','*','*','*'},{'*','*','*','*'}};
 
 
-// get name of player *
-string getNamePlayer() {
-
-    string namePlayer;
-    cout << "Enter your name : ";
-    cin >> namePlayer;
-
-    cout << endl;
-    return namePlayer;
-}
-
-
 // for show main table *
 void showMainTable() {
 
@@ -42,8 +30,8 @@ void showMainTable() {
     }
 
     // for clear console after 5 seconds *
-    // sleep(5);
-    // system("clear");
+    sleep(5);
+    system("clear");
 
 }
 
@@ -78,36 +66,65 @@ void getInput() {
         if (inputone >= 1 && inputone <= 16 && (inputone == 1 && inputtwo == 8) || (inputone == 8 && inputtwo == 1)) {
             table[0][0] = '1';
             table[1][3] = '1';
-            showPlayerTable();
         }
         else if (inputone >= 1 && inputone <= 16 && (inputone == 7 && inputtwo == 9) || (inputone == 9 && inputtwo == 7)) {
             table[1][2] = '2';
             table[2][0] = '2';
-            showPlayerTable();
+        }
+        else if (inputone >= 1 && inputone <= 16 && (inputone == 5 && inputtwo == 16) || (inputone == 16 && inputtwo == 5)) {
+            table[1][0] = '3';
+            table[3][3] = '3';
+        }
+        else if (inputone >= 1 && inputone <= 16 && (inputone == 6 && inputtwo == 14) || (inputone == 14 && inputtwo == 6)) {
+            table[1][1] = '4';
+            table[3][1] = '4';
+        }
+        else if (inputone >= 1 && inputone <= 16 && (inputone == 2 && inputtwo == 10) || (inputone == 10 && inputtwo == 2)) {
+            table[0][1] = '5';
+            table[2][1] = '5';
+        }
+        else if (inputone >= 1 && inputone <= 16 && (inputone == 4 && inputtwo == 11) || (inputone == 11 && inputtwo == 4)) {
+            table[0][3] = '6';
+            table[2][2] = '6';
+        }
+        else if (inputone >= 1 && inputone <= 16 && (inputone == 13 && inputtwo == 15) || (inputone == 15 && inputtwo == 13)) {
+            table[3][0] = '7';
+            table[3][2] = '7';
+        }
+        else if (inputone >= 1 && inputone <= 16 && (inputone == 3 && inputtwo == 12) || (inputone == 12 && inputtwo == 3)) {
+            table[0][2] = '8';
+            table[2][3] = '8';
         }else {
             cout << "Inputs are not Equal Value!" << endl;
         }
+        break;
     }
+}
+
+// for check home to full game *
+bool isFull() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (table[i][j] == '*') {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 
 int main(){
-    // getNamePlayer();
+
     showMainTable();
-    showPlayerTable();
-    getInput();
+    while (true) {
+        showPlayerTable();
+        getInput();
+        if (isFull()) {
+            showPlayerTable();
+            cout << "You Win!" << endl;
+            break;
+        }
+    }
     return 0;
 }
-
-
-
-
-//for(int i=0;i<4;i++){
-    //      for(int j=0;j<4;j++){
-    //        cout<<arr[i][j]<<" ";
-    //      }
-    //   }
-    //
-    //   if (arr[0][0] == 1) {
-    //       cout << "yes" << endl;
-    //      }
